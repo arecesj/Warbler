0d06f58235b1b8dcae8888ccd447d8cc41b2fd23
Sarah Kaplan <skaplan520@gmail.com>
2018-10-12 12:01:14 -0700

    passing tests for messages + likes + login

--- test_message_views.py
+++ test_message_views.py
@@ -8,19 +8,19 @@
 import os
 from unittest import TestCase
 
-from models import db, connect_db, Message, User
+from models import db, connect_db, Message, User, Like
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
 # before we import our app, since that will have already
 # connected to the database
 
-os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
+os.environ['DATABASE_URL'] = "postgresql:///warbler_test"
 
 
 # Now we can import app
 
-from app import app, CURR_USER_KEY
+from app import app, CURR_USER_KEY, do_logout
 
 # Create our tables (we do this here, so we only create the tables
 # once for all tests --- in each test, we'll delete the data
@@ -49,10 +49,15 @@ class MessageViewTestCase(TestCase):
                                     password="testuser",
                                     image_url=None)
 
+        self.testuser2 = User.signup(username="testuser2",
+                                     email="test2@test.com",
+                                     password="testuser2",
+                                     image_url=None)
+
         db.session.commit()
 
     def test_add_message(self):
-        """Can use add a message?"""
+        """Can user add a message?"""
 
         # Since we need to change the session to mimic logging in,
         # we need to use the changing-session trick:
@@ -71,3 +76,137 @@ class MessageViewTestCase(TestCase):
 
             msg = Message.query.one()
             self.assertEqual(msg.text, "Hello")
+
+    def test_single_message(self):
+        """Can you view a single message at message/<message_id>"""
+
+        # Since we need to change the session to mimic logging in,
+        # we need to use the changing-session trick:
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.testuser.id
+
+            c.post("/messages/new", data={"text": "Hello"})
+
+            msg = Message.query.one()
+
+            resp = c.get(f'/messages/{msg.id}')
+
+            self.assertIn(b'<ul class="list-group no-hover" id="messages">',
+                          resp.data)
+
+    def test_logged_in_delete_message(self):
+        """Can you delete a message at message/<message_id>/delete if logged in?"""
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.testuser.id
+
+            c.post("/messages/new", data={"text": "Hello"})
+
+            msg = Message.query.one()
+
+            resp = c.post(f'/messages/{msg.id}/delete')
+
+            msg_db = Message.query.first()
+
+            # after successful delete, page should redirect
+            self.assertEqual(resp.status_code, 302)
+
+            self.assertEqual(msg_db, None)
+
+    def test_logged_out_delete_message(self):
+        """If logged out user tries to delete message, should redirect to homepage"""
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.testuser.id
+
+            c.post("/messages/new", data={"text": "Hello"})
+
+            msg = Message.query.one()
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                del sess[CURR_USER_KEY]
+
+            resp = c.post(f'/messages/{msg.id}/delete', follow_redirects=True)
+
+            msg_db = Message.query.first()
+
+            # after successful delete, page should follow redirect
+            self.assertEqual(resp.status_code, 200)
+
+            # message in database should still exist
+            self.assertIn("Hello", msg_db.text)
+
+            # html response should contain message that access is denied
+            self.assertIn(b"Access unauthorized", resp.data)
+
+
+################################################
+# tests for likes
+
+    def test_like_message(self):
+        """Can user like a message?"""
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.testuser.id
+
+            # Now, that session setting is saved, so we can have
+            # the rest of ours test
+
+            c.post("/messages/new", data={"text": "Hello"})
+
+            # gets the user who is logged in (testuser)
+            liker = User.query.first()
+            msg_to_like = Message.query.first()
+
+            # testuser likes her own post
+            resp = c.post(f'like/{msg_to_like.id}',
+                          data={'return_to': f'/users/{liker.id}'}, follow_redirects=True)
+
+            # Make sure it follows redirect
+            self.assertEqual(resp.status_code, 200)
+
+            msg = Message.query.one()
+            self.assertEqual(msg.text, "Hello")
+            self.assertIn(b'<i class="fas fa-star pl-1">', resp.data)
+
+    def test_unlike_message(self):
+        """Can user unlike a message?"""
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.testuser.id
+
+            c.post("/messages/new", data={"text": "Hello"})
+
+            # gets the user who is logged in (testuser)
+            liker = User.query.first()
+            msg_to_like = Message.query.first()
+
+            # testuser likes her own post
+            c.post(f'like/{msg_to_like.id}',
+                   data={'return_to': f'/users/{liker.id}'}, follow_redirects=True)
+
+            # testuser unlikes her post
+            resp = c.post(f'unlike/{msg_to_like.id}',
+                          data={'return_to': f'/users/{liker.id}'}, follow_redirects=True)
+
+            # Make sure it follows redirect
+            self.assertEqual(resp.status_code, 200)
+
+            msg = Message.query.one()
+            self.assertEqual(msg.text, "Hello")
+            self.assertIn(b'<i class="far fa-star pl-1">', resp.data)
+
+    def test_404(self):
+        """Does 404 page load"""
+
+        resp = self.client.get('/fakeroute')
+
+        self.assertEqual(resp.status_code, 404)
+        self.assertIn(b'404. Page not found', resp.data)
--- test_user_views.py
+++ test_user_views.py
@@ -8,7 +8,7 @@
 import os
 from unittest import TestCase
 
-from models import db, User, Message, FollowersFollowee, Like
+from models import db, connect_db, User, Message, FollowersFollowee, Like
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -20,13 +20,28 @@ os.environ["DATABASE_URL"] = "postgresql:///warbler_test"
 
 # Now we can import app
 
-from app import app, signup
+from app import app, CURR_USER_KEY
 
 # Create our tables (we do this here, so we only create the tables
 # once for all tests --- in each test, we'll delete the data
 # and create fresh new clean test data
 
 db.create_all()
+
+# Don't have WTForms use CSRF at all, since it's a pain to test
+
+app.config['WTF_CSRF_ENABLED'] = False
+
+
+def sign_up_user():
+    """adds test user to database"""
+
+    user = User.signup(username='JuanTon', email='test@test.com',
+                       password='abc123', image_url='http://google.com')
+    db.session.add(user)
+    db.session.commit()
+    user_in_db = User.query.first()
+    return user_in_db
 
 
 class UserViewsTestCase(TestCase):
@@ -35,11 +50,14 @@ class UserViewsTestCase(TestCase):
     def setUp(self):
         """Create test client, add sample data."""
 
-        FollowersFollowee.query.delete()
-        Message.query.delete()
         User.query.delete()
+        Message.query.delete()
+        FollowersFollowee.query.delete()
+        Like.query.delete()
 
         self.client = app.test_client()
+
+        db.session.commit()
 
     def test_get_signup(self):
         """Does signup route work for get request?"""
@@ -59,13 +77,8 @@ class UserViewsTestCase(TestCase):
         data = {'username': 'JuanTon', 'password': 'abc123',
                 'email': 'test@test.com', 'image_url': 'http://google.com'}
 
+        get_request = self.client.get('/signup')
         result = self.client.post('/signup', data=data, follow_redirects=True)
-
-        # with self.client:
-        #     result = self.client.post(
-        #         '/signup', data=data, follow_redirects=True)
-        #     self.assertTrue(session['curr_user'])
-
         user_in_db = User.query.first()
 
         # Page 200s
@@ -75,5 +88,62 @@ class UserViewsTestCase(TestCase):
         self.assertEquals(user_in_db.username, "JuanTon")
 
         # Post to signup page returns correct HTML
-        # self.assertIn(
-        #     b'<ul class="user-stats nav nav-pills">', result.data)
+        self.assertIn(
+            b'<ul class="user-stats nav nav-pills">', result.data)
+
+    def test_valid_login(self):
+        """Valid login should add user_id to session and redirect home"""
+
+        user = sign_up_user()
+
+        resp = self.client.post(
+            '/login', data={'username': user.username, 'password': 'abc123'}, follow_redirects=True)
+
+        # The user should be added to the session
+        with self.client as c:
+            with c.session_transaction() as sess:
+                result = sess[CURR_USER_KEY]
+
+                self.assertEqual(result, user.id)
+
+        # Page 200s
+        self.assertEquals(resp.status_code, 200)
+
+        # Post to login returns correct HTML
+        self.assertIn(
+            b'<a href="/logout">Log out</a>', resp.data)
+
+    def test_invalid_login(self):
+        """Invalid login should render users/login.html with message of Invalid credentials."""
+
+        resp = self.client.post(
+            '/login', data={'username': 'SlytherinSilas', 'password': 'wrongpw'}, follow_redirects=True)
+
+        # Page 200s
+        self.assertEquals(resp.status_code, 200)
+
+        # Invalid login returns flashed message of invalid credentials
+        self.assertIn(
+            b'Invalid credentials.', resp.data)
+
+    def test_logout(self):
+        """Logout should remove user_id from session and redirect to /login"""
+
+        user = sign_up_user()
+
+        resp = self.client.post(
+            '/login', data={'username': user.username, 'password': 'abc123'}, follow_redirects=True)
+
+        # The user should be added to the session
+        with self.client as c:
+            with c.session_transaction() as sess:
+                result = sess[CURR_USER_KEY]
+
+        self.client.get('/logout', follow_redirects=True)
+
+        # Page 200s
+        self.assertEquals(resp.status_code, 200)
+
+        # Post to login returns correct HTML
+        self.assertIn(
+            b'You have successfully logged out', resp.data)
